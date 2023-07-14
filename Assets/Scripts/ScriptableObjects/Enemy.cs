ba8a3d5f43ec0265b8587bb5cea2da975e763870
ObjectOops <92550885+ObjectOops@users.noreply.github.com>
2023-07-14 14:28:41 -0500

    Update .gitignore and switch to LF.
    Co-authored-by: Vespertilo <Vespertilo09@gmail.com>

---
+++ Assets/Scripts/ScriptableObjects/Enemy.cs
@@ -0,0 +1,10 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "Enemy", menuName = "ScriptableObject/Enemy")]
+public class Enemy : ScriptableObject
+{
+
+}
+
