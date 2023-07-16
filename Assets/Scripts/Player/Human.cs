9782c9fd500a1166a1fb515a25617db89183d25b
ObjectOops <92550885+ObjectOops@users.noreply.github.com>
2023-07-16 14:35:36 -0500

    Primitive fall mechanics.
    Co-authored-by: Vespertilo <Vespertilo09@gmail.com>

--- Assets/Scripts/Player/Human.cs
+++ Assets/Scripts/Player/Human.cs
@@ -4,14 +4,49 @@ using UnityEngine;
 
 public class Human : MonoBehaviour
 {
-	public bool isTargeted;
+	public bool isTargeted, onGround;
+	public float frameFallDistance, fallDamageDistance, groundElevationY;
 	public MutantEnemy mutantPrefab;
 	public Transform scroller;
 
-	public void SpawnMutant()
+	private float distanceFallen = 0;
+
+    void Update()
+    {
+		onGround = OnGround();
+		if (!onGround && !isTargeted)
+        {
+			Fall();
+        }
+		else if (onGround)
+        {
+			TestFallDamage();
+        }
+    }
+
+    public void SpawnMutant()
 	{
 		Instantiate(mutantPrefab, transform.position, Quaternion.identity, FindObjectOfType<ScrollManager>().transform);
 		Destroy(this.gameObject);
+	}
+
+	public bool OnGround()
+    {
+		return transform.position.y <= groundElevationY;
+    }
+
+	private void Fall()
+    {
+		transform.position = new Vector2(transform.position.x, transform.position.y - frameFallDistance);
+		distanceFallen += frameFallDistance;
+	}
+
+	private void TestFallDamage()
+    {
+		if (distanceFallen >= fallDamageDistance)
+		{
+			Destroy(this.gameObject);
+		}
 	}
 
 }
